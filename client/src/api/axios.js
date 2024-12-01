106f37d0c278818ff6567a2dd642ef971f55efef
Guze <velardereynald@gmail.com>
2024-12-01 13:45:35 +0800

    1

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fallbackURL = "https://web-production-dd1d.up.railway.app/api";
-
+//UPDATE
 // const API_URL = import.meta.env.VITE_API_URL;
 const API_URL = fallbackURL;
 
