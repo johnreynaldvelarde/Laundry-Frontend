332e708d3291871bb9b16ce4943797b42e6fc1db
Admin <veladereynald@gmail.com>
2024-12-01 13:31:57 +0800

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -21,14 +21,11 @@ export const axiosPrivate = axios.create({
   withCredentials: true,
 });
 
-// Add request and response interceptors
 axiosPrivate.interceptors.request.use(
   (config) => {
-    // You can add authorization headers or other configurations here
     return config;
   },
   (error) => {
-    // Handle request error here
     return Promise.reject(error);
   }
 );
@@ -38,7 +35,6 @@ axiosPrivate.interceptors.response.use(
     return response;
   },
   (error) => {
-    // Log only if it's not a 400 error
     if (error.response && error.response.status !== 400) {
       console.error("API Error:", error.response || error.message);
     }
