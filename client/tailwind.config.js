6b74124973338a1190d3aaee93b4d67dc5c3f963
Guze <velardereynald@gmail.com>
2024-12-08 06:02:30 +0800

    update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,3 @@
-/*
-/** @type {import('tailwindcss').Config} 
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-*/
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
@@ -42,37 +32,27 @@ module.exports = {
       },
     },
     animation: {
-      'running-color-1': 'runningColor1 1s linear infinite',
-      'running-color-2': 'runningColor2 1s linear infinite',
-      'running-color-3': 'runningColor3 1s linear infinite',
+      "running-color-1": "runningColor1 1s linear infinite",
+      "running-color-2": "runningColor2 1s linear infinite",
+      "running-color-3": "runningColor3 1s linear infinite",
     },
     keyframes: {
       runningColor1: {
-        '0%': { backgroundColor: '#D3D3D3' }, // Gray
-        '50%': { backgroundColor: '#38a169' }, // Green
-        '100%': { backgroundColor: '#e53e3e' }, // Red
-        
+        "0%": { backgroundColor: "#D3D3D3" }, // Gray
+        "50%": { backgroundColor: "#38a169" }, // Green
+        "100%": { backgroundColor: "#e53e3e" }, // Red
       },
       runningColor2: {
-        '0%': { backgroundColor: '#38a169' }, // Green
-        '50%': { backgroundColor: '#dd6b20' }, // Orange
-        '100%': { backgroundColor: '#e53e3e' }, // Red
+        "0%": { backgroundColor: "#38a169" }, // Green
+        "50%": { backgroundColor: "#dd6b20" }, // Orange
+        "100%": { backgroundColor: "#e53e3e" }, // Red
       },
       runningColor3: {
-        '0%': { backgroundColor: '#D3D3D3' }, // Gray
-        '50%': { backgroundColor: '#e53e3e' }, // Red
-        '100%': { backgroundColor: '#38a169' }, // Green
+        "0%": { backgroundColor: "#D3D3D3" }, // Gray
+        "50%": { backgroundColor: "#e53e3e" }, // Red
+        "100%": { backgroundColor: "#38a169" }, // Green
       },
     },
   },
   plugins: [],
 };
-
-// screens: {
-//   xs: "480px",
-//   ss: "620px",
-//   sm: "768px",
-//   md: "1060px",
-//   lg: "1200px",
-//   xl: "1700px",
-// },
