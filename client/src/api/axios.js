8fefc6a6883eec150144d2edff34938521c1629e
Guze <velardereynald@gmail.com>
2024-12-08 05:57:12 +0800

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const fallbackURL = "https://web-production-dd1d.up.railway.app/api";
 //UPDATE
-const API_URL = import.meta.env.VITE_API_URL;
-//const API_URL = fallbackURL;
+//const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = fallbackURL;
 
 export const axiosPublic = axios.create({
   baseURL: API_URL,
